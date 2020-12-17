ea2e440b9992e34183dbfe7bc728d3c52ab0a1bd
Adyasha Mohanty <adyashamohanty8105@gmail.com>
2020-12-17 13:57:49 +0530

    MyFrame.java

---
+++ AWT/MyFrame.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class MyFrame {
+    public static void main (String args[]) {
+        Frame frame = new Frame ("Frame in Java");
+        frame.resize (500,500);
+        frame.setBackground (Color.cyan);
+        frame.show();
+   }
+}
